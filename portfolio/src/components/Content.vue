e76ee0b1c5f30bbe3e1932414ab249423c906d87
Kabi <34021457+ksivanamam@users.noreply.github.com>
2021-08-18 22:38:51 +0200

    added tech stack

--- portfolio/src/components/Content.vue
+++ portfolio/src/components/Content.vue
@@ -1,51 +1,156 @@
 <template>
 	<div class="content">
 		<div>
-			<h1 class="title">{{ german.title }}</h1>
+			<h1 v-if="german" class="title" @click="changeLanguage">{{ germanContent.title }}
+			</h1>
+			<h1 v-else class="title" @click="changeLanguage">{{ englishContent.title }}</h1>
 		</div>
 		<div>
-			<h2 class="subTitle anim-typewriter">{{ german.subTitle }}</h2>
+			<h2 v-if="german" class="subTitle anim-typewriter">{{ germanContent.subTitle }}</h2>
+			<h2 v-else class="subTitle anim-typewriter">{{ englishContent.subTitle }}</h2>
 		</div>
-		<p v-for="p in german.paragraphs" :key="p.id">
-			{{  p.text }}
-		</p>
-		<a v-for="l in german.links" :key="l.id" :href="l.page" target="_blank" :id="l.style">
-			{{ l.text }}
-		</a>
+		<div v-if="german">
+			<p v-for="p in germanContent.paragraphs" :key="p.id">
+				{{  p.text }}
+			</p>
+		</div>
+		<div v-else>
+			<p v-for="p in englishContent.paragraphs" :key="p.id">
+				{{  p.text }}
+			</p>
+		</div>
+		<div v-if="german">
+			<a v-for="l in germanContent.links" :key="l.id" :href="l.page" target="_blank" :id="l.style">
+				{{ l.text }}
+			</a>
+		</div>
+		<div v-else>
+			<a v-for="l in englishContent.links" :key="l.id" :href="l.page" target="_blank" :id="l.style">
+				{{ l.text }}
+			</a>
+		</div>
+		<div class="stack">
+			<div v-if="german">
+				<p>Mein Technology Stack:</p>
+			</div>
+			<div v-else>
+				<p>My Technology stack:</p>
+			</div>
+			<div v-if="german">
+				<div v-for="t in tags" :key="t-id" class="tag"
+					:style="{ color: t.color, backgroundColor: t.backgroundColor }">
+					{{ t.name }}
+				</div>
+			</div>
+			<div v-else>
+				<div v-for="t in tags" :key="t-id" class="tag"
+					:style="{ color: t.color, backgroundColor: t.backgroundColor }">
+					{{ t.name }}
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
-<script setup>
-	import {
+<script>
+	/*import {
 		reactive
-	} from 'vue'
+	} from 'vue'*/
 
-
-	const german = reactive({
-		title: 'Hi :)',
-		subTitle: 'Ich heisse Kabilan Sivanamam.',
-		paragraphs: [{
-			id: 1,
-			text: 'Ich bin ein ausgebildeter Betriebsinformatiker und ein absoluter Informatik-Allrounder. Ich packe gerne an bei der Einrichtung von Netzwerken, arbeite aber auch gerne vor dem Laptop und schreibe Code.'
-		}, {
-			id: 2,
-			text: `Im Moment arbeite ich hauptsächlich an kleinen privaten Projekten wie die Entwicklung von Discord Bots und einer Sport-App.`
-		}, {
-			id: 3,
-			text: 'In meiner Freizeit sitze ich aber nicht nur vor dem Laptop. Wenn ich gerade nicht in der Emme schwimme, gehe ich gerne klettern oder spiele Gitarre. 🎸'
-		}],
-		links: [{
-			id: 1,
-			text: 'LinkedIn',
-			page: 'https://www.linkedin.com/in/kabilan-sivanamam-3607b5172/',
-			style: 'linkedIn'
-		}, {
-			id: 2,
-			text: 'GitHub',
-			page: 'https://github.com/ksivanamam',
-			style: 'gitHub'
-		}]
-	})
+	export default {
+		data() {
+			return {
+				german: true,
+				germanContent: {
+					title: 'Hi :)',
+					subTitle: 'Ich heisse Kabilan Sivanamam.',
+					paragraphs: [{
+						id: 1,
+						text: 'Ich bin ein ausgebildeter Betriebsinformatiker und ein Informatik-Allrounder. Ich packe gerne an bei der Einrichtung von Netzwerken, arbeite aber auch gerne vor dem Laptop und schreibe Code.'
+					}, {
+						id: 2,
+						text: `Im Moment arbeite ich hauptsächlich an kleinen privaten Projekten wie die Entwicklung von Discord Bots und einer Sport-App.`
+					}, {
+						id: 3,
+						text: 'In meiner Freizeit sitze ich aber nicht nur vor dem Laptop. Wenn ich gerade nicht in der Emme schwimme, gehe ich gerne klettern oder spiele Gitarre. 🎸'
+					}],
+					links: [{
+						id: 1,
+						text: 'LinkedIn',
+						page: 'https://www.linkedin.com/in/kabilan-sivanamam-3607b5172/',
+						style: 'linkedIn'
+					}, {
+						id: 2,
+						text: 'GitHub',
+						page: 'https://github.com/ksivanamam',
+						style: 'gitHub'
+					}]
+				},
+				englishContent: {
+					title: 'Hi :)',
+					subTitle: 'My name is Kabilan Sivanamam.',
+					paragraphs: [{
+						id: 1,
+						text: `I studied computer science and I'm an IT allrounder. I like to get my hands dirty while working on network setups but I also like to cozy up and write code on my computer.`
+					}, {
+						id: 2,
+						text: `Currently I'm working on small projects like a private and a public Discord bot and I'm also developing a workout app.`
+					}, {
+						id: 3,
+						text: `But I'm not always working on stuff. In my free time I like to swim, climb, and play the guitar. 🎸`
+					}],
+					links: [{
+						id: 1,
+						text: 'LinkedIn',
+						page: 'https://www.linkedin.com/in/kabilan-sivanamam-3607b5172/',
+						style: 'linkedIn'
+					}, {
+						id: 2,
+						text: 'GitHub',
+						page: 'https://github.com/ksivanamam',
+						style: 'gitHub'
+					}]
+				},
+				tags: [{
+					id: 1,
+					name: '#vue',
+					color: '#fff',
+					backgroundColor: '#42b883'
+				}, {
+					id: 3,
+					name: '#vuetify',
+					color: '#fff',
+					backgroundColor: '#89CFF0'
+				}, {
+					id: 4,
+					name: '#node',
+					color: '#fff',
+					backgroundColor: '#6cc24a'
+				}, {
+					id: 5,
+					name: '#mysql',
+					color: '#fff',
+					backgroundColor: '#00758f'
+				}, {
+					id: 6,
+					name: '#firebase',
+					color: '#fff',
+					backgroundColor: '#FFCA28'
+				}, {
+					id: 7,
+					name: '#knex',
+					color: '#fff',
+					backgroundColor: '#FFA500'
+				}]
+			}
+		},
+		methods: {
+			changeLanguage() {
+				console.log(this.german)
+				this.german = !this.german
+			}
+		}
+	}
 </script>
 
 <style scoped>
